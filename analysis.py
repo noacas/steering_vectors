330b1fe43ed4ba117736cc647f36f70d144ac7af
Tal <talargov1@mail.tau.ac.il>
2025-07-08 17:25:07 +0300

    added-normalizations

--- analysis.py
+++ analysis.py
@@ -6,22 +6,42 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import collections
 import pandas as pd
+import torch
 
 from consts import MIN_LEN
-from dot_act import get_dot_act, get_mean_dot_prod
+from dot_act import get_dot_act, get_mean_dot_prod, get_act
 from model import ModelBundle
 from utils import dict_subtraction
 
 
-def compute_component_prediction_r2(dot_prod_dict_train, dot_prod_dict_test, residual_stream_component_name = 'blocks.10.hook_resid_pre'):
+def compute_component_prediction_r2(dot_prod_dict_train, dot_prod_dict_test, dot_prod_dict_train_norm = None, dot_prod_dict_test_norm = None, residual_stream_component_name = 'blocks.10.hook_resid_pre'):
     # Expecting that dot_prod_dict is a nested dict of example: {component: dot product} pairs
+    assert dot_prod_dict_train_norm is None or dot_prod_dict_train_norm is not None, "If you provide dot_prod_dict_train_norm, you must also provide dot_prod_dict_test_norm"
+    normalize = dot_prod_dict_train_norm is not None
+
     component_names = dot_prod_dict_train[0].keys()
     total_dot_products_train = np.array([dot_prod_dict_train[example][residual_stream_component_name] for example in range(len(dot_prod_dict_train))])
     total_dot_products_test = np.array([dot_prod_dict_test[example][residual_stream_component_name] for example in range(len(dot_prod_dict_test))])
+    if normalize:
+        total_norm_train = np.array([dot_prod_dict_train_norm[example][residual_stream_component_name] for example in range(len(dot_prod_dict_train))])
+        total_norm_test = np.array([dot_prod_dict_test_norm[example][residual_stream_component_name] for example in range(len(dot_prod_dict_test))])
+
+        # Normalize the total dot products
+        total_dot_products_train /= total_norm_train
+        total_dot_products_test /= total_norm_test
+
     r2s = dict()
     for component_name in component_names:
-        component_dot_products_train = np.array([dot_prod_dict_train[example][component_name] for example in range(len(dot_prod_dict_train))])
-        component_dot_products_test = np.array([dot_prod_dict_test[example][component_name] for example in range(len(dot_prod_dict_test))])
+        component_norm_train = np.array([dot_prod_dict_train_norm[example][component_name] for example in range(len(dot_prod_dict_train))])
+        component_norm_test = np.array([dot_prod_dict_test_norm[example][component_name] for example in range(len(dot_prod_dict_test))])
+
+        if normalize:
+            component_dot_products_train = np.array([dot_prod_dict_train[example][component_name] for example in range(len(dot_prod_dict_train))])
+            component_dot_products_test = np.array([dot_prod_dict_test[example][component_name] for example in range(len(dot_prod_dict_test))])
+
+            # Normalize the component dot products
+            component_dot_products_train /= component_norm_train
+            component_dot_products_test /= component_norm_test
 
         lr = LinearRegression()
         lr.fit(component_dot_products_train.reshape(-1, 1), total_dot_products_train)
@@ -31,11 +51,22 @@ def compute_component_prediction_r2(dot_prod_dict_train, dot_prod_dict_test, res
     return r2s
 
 
-def compute_multi_component_prediction_r2(dot_prod_dict_train, dot_prod_dict_test, residual_stream_component_name = 'blocks.10.hook_resid_pre'):
+def compute_multi_component_prediction_r2(dot_prod_dict_train, dot_prod_dict_test, dot_prod_dict_train_norm = None, dot_prod_dict_test_norm = None, residual_stream_component_name = 'blocks.10.hook_resid_pre'):
+    assert dot_prod_dict_train_norm is None or dot_prod_dict_train_norm is not None, "If you provide dot_prod_dict_train_norm, you must also provide dot_prod_dict_test_norm"
+    normalize = dot_prod_dict_train_norm is not None
+
     # Expecting that dot_prod_dict is a nested dict of example: {component: dot product} pairs
     component_names = dot_prod_dict_train[0].keys()
     total_dot_products_train = np.array([dot_prod_dict_train[example][residual_stream_component_name] for example in range(len(dot_prod_dict_train))])
     total_dot_products_test = np.array([dot_prod_dict_test[example][residual_stream_component_name] for example in range(len(dot_prod_dict_test))])
+    if normalize:
+        total_norm_train = np.array([dot_prod_dict_train_norm[example][residual_stream_component_name] for example in range(len(dot_prod_dict_train))])
+        total_norm_test = np.array([dot_prod_dict_test_norm[example][residual_stream_component_name] for example in range(len(dot_prod_dict_test))])
+
+        # Normalize the total dot products
+        total_dot_products_train /= total_norm_train
+        total_dot_products_test /= total_norm_test
+
     r2s = {}
 
     for c1, c2 in itertools.combinations(component_names, 2):
@@ -48,12 +79,71 @@ def compute_multi_component_prediction_r2(dot_prod_dict_train, dot_prod_dict_tes
         component_dot_products_train = np.stack((c1_dot_products_train, c2_dot_products_train), axis=1)
         component_dot_products_test = np.stack((c1_dot_products_test, c2_dot_products_test), axis=1)
 
+        if normalize:
+            c1_norm_train = np.array([dot_prod_dict_train_norm[example][c1] for example in range(len(dot_prod_dict_train))])
+            c1_norm_test = np.array([dot_prod_dict_test_norm[example][c1] for example in range(len(dot_prod_dict_test))])
+
+            c2_norm_train = np.array([dot_prod_dict_train_norm[example][c2] for example in range(len(dot_prod_dict_train))])
+            c2_norm_test = np.array([dot_prod_dict_test_norm[example][c2] for example in range(len(dot_prod_dict_test))])
+
+            # Normalize the component dot products
+            component_dot_products_train /= np.stack((c1_norm_train, c2_norm_train), axis=1)
+            component_dot_products_test /= np.stack((c1_norm_test, c2_norm_test), axis=1)
+
         pipeline = LinearRegression()
         pipeline.fit(component_dot_products_train.reshape(-1, 1), total_dot_products_train)
         pred = pipeline.predict(component_dot_products_test.reshape(-1, 1))
 
         r2s[f"{c1} x {c2}"] = r2_score(total_dot_products_test, pred)
     return r2s
+
+
+def compare_component_diff_in_means(model_bundle: ModelBundle, multicomponent=False):
+    train_dict = collections.defaultdict(dict)
+    test_dict = collections.defaultdict(dict)
+
+    for pos in range(-1, -MIN_LEN - 1, -1):
+        print("pos = ", pos)
+        subset_len = len(model_bundle.harmful_inst_train) # There is a great imbalance in dataset size
+
+        model = model_bundle.model
+        refusal_direction = model_bundle.refusal_direction
+
+        harmless_outputs_train = get_act(model, model_bundle.harmless_inst_train[:subset_len], pos)
+        harmful_outputs_train = get_act(model, model_bundle.harmful_inst_train[:subset_len], pos)
+        harmless_outputs_test = get_act(model, model_bundle.harmless_inst_test[:subset_len], pos)
+        harmful_outputs_test = get_act(model, model_bundle.harmful_inst_test[:subset_len], pos)
+
+        diff_in_means_train = get_mean_dot_prod(harmful_outputs_train)
+        diff_in_means_train = dict_subtraction(diff_in_means_train, get_mean_dot_prod(harmless_outputs_train))
+        for component_name in diff_in_means_train:
+            diff_in_means_train[component_name] = torch.nn.functional.normalize(diff_in_means_train[component_name])
+            diff_in_means_train[component_name] = torch.dot(diff_in_means_train[component_name], refusal_direction.type(diff_in_means_train[component_name].dtype)).item()
+
+        diff_in_means_test = get_mean_dot_prod(harmful_outputs_test)
+        diff_in_means_test = dict_subtraction(diff_in_means_test, get_mean_dot_prod(harmless_outputs_test))
+        for component_name in diff_in_means_test:
+            diff_in_means_test[component_name] = torch.nn.functional.normalize(diff_in_means_test[component_name])
+            diff_in_means_test[component_name] = torch.dot(diff_in_means_test[component_name], refusal_direction.type(diff_in_means_test[component_name].dtype)).item()
+
+        # Sort the diff in means by train
+        diff_in_means_train_list = list(diff_in_means_train.items())
+        diff_in_means_train_list.sort(key=lambda x: abs(x[1]), reverse=True)
+
+        for component_name, _ in diff_in_means_train_list:
+            diff_train = diff_in_means_train[component_name]
+            diff_test = diff_in_means_test[component_name]
+
+            train_dict[pos][component_name] = diff_train
+            test_dict[pos][component_name] = diff_test
+            print(f"{component_name}: Diff in means - Train: {diff_train:.4f} Test: {diff_test:.4f}")
+            print()
+
+
+    train_df = pd.DataFrame(train_dict)
+    test_df = pd.DataFrame(test_dict)
+    train_df.to_csv(os.path.join(model_bundle.results_dir, 'train_df.csv'))
+    test_df.to_csv(os.path.join(model_bundle.results_dir, 'test_df.csv'))
 
 
 def compare_component_prediction_r2(model_bundle: ModelBundle, multicomponent=False):
--- dot_act.py
+++ dot_act.py
@@ -4,13 +4,9 @@ import copy
 import torch
 from tqdm import tqdm
 
-from consts import LAYER
+from consts import LAYER, DEVICE
 
-def get_dot_act(model, dataset, pos, refusal_dir):
-    """
-    Cache dot products with the steering vector for each example in the dataset at a given position along the sequence.
-    """
-
+def get_act(model, dataset, pos):
     print("\nProcessing dataset and caching activations...")
     output = []
     hook_names = [f"blocks.{i}.hook_attn_out" for i in range(LAYER)]
@@ -22,14 +18,46 @@ def get_dot_act(model, dataset, pos, refusal_dir):
     for text in tqdm(dataset, desc="Caching Activations"):
         # Tokenize the input text
         # TODO: For the Qwen example, they had some chat template, do we need this too maybe?
-        inputs = model.to_tokens(text).to(model.device)
+        inputs = model.to_tokens(text).to(DEVICE)
 
         with torch.no_grad():
           #TODO next meeting shira
           # This is from the tutorial, we don't need to save all the layers in the cache
           # _, gpt2_attn_cache = model.run_with_cache(gpt2_tokens, remove_batch_dim=True, stop_at_layer=attn_layer + 1, names_filter=[attn_hook_name])
-            _, _cache = model.run_with_cache(inputs, names_filter = hook_names, stop_at_layer=attn_layer + 1)
+            _, _cache = model.run_with_cache(inputs, names_filter = hook_names, stop_at_layer=LAYER + 1)
+            cache = _cache.cpu()[0, pos] # Need to change the indexing when adding batches
+
+            output.append(cache)
+
+     # output[example][component_name] = dot product of component component_name with refusal dir for text example i at position pos
+    return output
+
+def get_dot_act(model, dataset, pos, refusal_dir, cache_norms=False):
+    """
+    Cache dot products with the steering vector for each example in the dataset at a given position along the sequence.
+    """
+
+    print("\nProcessing dataset and caching activations...")
+    output_prod, output_norm = [], []
+    hook_names = [f"blocks.{i}.hook_attn_out" for i in range(LAYER)]
+    hook_names.extend([f"blocks.{i}.hook_mlp_out" for i in range(LAYER)])
+    hook_names.append(f"blocks.{LAYER}.hook_resid_pre")
+    hook_names.append(f"blocks.0.hook_resid_pre")
+
+    # TODO: Batching
+    for text in tqdm(dataset, desc="Caching Activations"):
+        # Tokenize the input text
+        # TODO: For the Qwen example, they had some chat template, do we need this too maybe?
+        inputs = model.to_tokens(text).to(DEVICE)
+
+        with torch.no_grad():
+          #TODO next meeting shira
+          # This is from the tutorial, we don't need to save all the layers in the cache
+          # _, gpt2_attn_cache = model.run_with_cache(gpt2_tokens, remove_batch_dim=True, stop_at_layer=attn_layer + 1, names_filter=[attn_hook_name])
+            _, _cache = model.run_with_cache(inputs, names_filter = hook_names, stop_at_layer=LAYER + 1)
             cache = copy.deepcopy(_cache.cache_dict)
+            if cache_norms:
+                norm_cache = {}
 
             for component_name in cache.keys():
                 component_dot_product = torch.einsum(
@@ -38,6 +66,8 @@ def get_dot_act(model, dataset, pos, refusal_dir):
                         refusal_dir.type(cache[component_name].dtype),
                     ).detach().cpu()[0 , pos].item() # Need to change the indexing when adding batches
                 cache[component_name] = component_dot_product
+                if cache_norms:
+                    norm_cache[component_name] = torch.norm(cache[component_name], dim=-1).detach().cpu()[0, pos].item()
 
             # Remove from GPU to avoid OOM
             # This is probably bad and slow, FIX ME
@@ -45,16 +75,17 @@ def get_dot_act(model, dataset, pos, refusal_dir):
             del _cache
             # gc.collect()
 
-            output.append(cache)
+            if cache_norms:
+                output_norm.append(norm_cache)
+            output_prod.append(cache)
 
      # output[example][component_name] = dot product of component component_name with refusal dir for text example i at position pos
-    return output
-
+    return output_prod, norm_cache if cache_norms else output_prod
 
 def get_mean_dot_prod(dot_prod_dict):
     component_names = dot_prod_dict[0].keys()
     means = dict()
     for component_name in component_names:
         all_dot_products = torch.tensor([dot_prod_dict[example][component_name] for example in range(len(dot_prod_dict))])
-        means[component_name] = torch.mean(all_dot_products)
+        means[component_name] = torch.mean(all_dot_products, dim=0)
     return means
