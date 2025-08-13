667b9667838858ef899a476e27350f9d934fa53c
Tal <talargov1@mail.tau.ac.il>
2025-08-13 15:19:28 +0300

    layer-changes2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Where do steering vectors come from
 """
 from analysis import compare_component_prediction_r2, predict_dot_product_lasso
 from model import ModelBundle
-from consts import GEMMA, GEMMA2
+from consts import GEMMA, GEMMA2, LAYER_GEMMA1, LAYER_GEMMA2
 import os
 import re
 from arg_parser import parse_args
@@ -26,12 +26,14 @@ def main():
 
     # GEMMA and harmfull steering vector
     if GEMMA in args.models:
+        LAYER = LAYER_GEMMA1
         model_bundle.load_model(GEMMA)
         model_bundle.load_steering_vector("harmfull")
         predict_dot_product_lasso(model_bundle=model_bundle)
 
     # for GEMMA2 all steering vectors in dir content/axbench_chosen_dataset/
     if GEMMA2 in args.models:
+        LAYER = LAYER_GEMMA2
         model_bundle.load_model(GEMMA2)
         for steering_vector in get_steering_vector_names_for_gemma2():
             model_bundle.load_steering_vector(steering_vector)
