4a842bd9094fef39ac170d781556a7b258c8493c
Tal <talargov1@mail.tau.ac.il>
2025-08-13 15:16:13 +0300

    layer-changes

--- consts.py
+++ consts.py
@@ -10,17 +10,18 @@ if torch.cuda.is_available():
     DEVICE = 'cuda'
 # elif torch.backends.mps.is_available():
 #     DEVICE = 'mps'
-LAYER = 10  # The location is taken from the corresponding json from the project's source
+LAYER_GEMMA1 = 10  # The location is taken from the corresponding json from the project's source
+LAYER_GEMMA2 = 20
 MIN_LEN = 3  # Minimum length of the instruction to consider
 
 GEMMA_2_HOOK_NAMES = (
-            [f"blocks.{i}.ln1_post.hook_normalized" for i in range(LAYER)] +
-            [f"blocks.{i}.ln2_post.hook_normalized" for i in range(LAYER)] +
-            [f"blocks.{LAYER}.hook_resid_pre", "blocks.0.hook_resid_pre"]
+            [f"blocks.{i}.ln1_post.hook_normalized" for i in range(LAYER_GEMMA2)] +
+            [f"blocks.{i}.ln2_post.hook_normalized" for i in range(LAYER_GEMMA2)] +
+            [f"blocks.{LAYER_GEMMA2}.hook_resid_pre", "blocks.0.hook_resid_pre"]
     ) 
 
 GEMMA_1_HOOK_NAMES = hook_names = (
-            [f"blocks.{i}.hook_attn_out" for i in range(LAYER)] +
-            [f"blocks.{i}.hook_mlp_out" for i in range(LAYER)] +
-            [f"blocks.{LAYER}.hook_resid_pre", "blocks.0.hook_resid_pre"]
+            [f"blocks.{i}.hook_attn_out" for i in range(LAYER_GEMMA1)] +
+            [f"blocks.{i}.hook_mlp_out" for i in range(LAYER_GEMMA1)] +
+            [f"blocks.{LAYER_GEMMA1}.hook_resid_pre", "blocks.0.hook_resid_pre"]
     )
