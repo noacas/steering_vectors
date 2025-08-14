b5f31f17fd9a5d100a7a16067d600f42bec1052c
Moran Zaks <moranzaks@Morans-MacBook-Air.local>
2025-08-14 15:48:23 +0300

    start organize

--- consts.py
+++ consts.py
@@ -1,28 +1,32 @@
 import torch
 
-GEMMA_MODEL_PATH = 'google/gemma-2b-it'
-GEMMA_2_MODEL_PATH = 'google/gemma-2-2b-it'
-GEMMA = "gemma"
-GEMMA2 = "gemma2"
+# --- General --- #
 HF_TOKEN = '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'
+MIN_LEN = 3  # Minimum length of the instruction to consider
+
+# --- Device --- #
 DEVICE = 'cpu'
 if torch.cuda.is_available():
     DEVICE = 'cuda'
 # elif torch.backends.mps.is_available():
 #     DEVICE = 'mps'
-LAYER_GEMMA1 = 10  # The location is taken from the corresponding json from the project's source
-LAYER_GEMMA2 = 20
-LAYER = LAYER_GEMMA2 # TODO: change to LAYER_GEMMA1 for gemma1
-MIN_LEN = 3  # Minimum length of the instruction to consider
 
-GEMMA_2_HOOK_NAMES = (
-            [f"blocks.{i}.ln1_post.hook_normalized" for i in range(LAYER_GEMMA2)] +
-            [f"blocks.{i}.ln2_post.hook_normalized" for i in range(LAYER_GEMMA2)] +
-            [f"blocks.{LAYER_GEMMA2}.hook_resid_pre", "blocks.0.hook_resid_pre"]
-    ) 
-
+# --- Gemma --- #
+GEMMA_1 = "gemma"
+GEMMA_1_MODEL_PATH = 'google/gemma-2b-it'
+GEMMA_1_LAYER = 10  # The location is taken from the corresponding json from the project's source
 GEMMA_1_HOOK_NAMES = hook_names = (
-            [f"blocks.{i}.hook_attn_out" for i in range(LAYER_GEMMA1)] +
-            [f"blocks.{i}.hook_mlp_out" for i in range(LAYER_GEMMA1)] +
-            [f"blocks.{LAYER_GEMMA1}.hook_resid_pre", "blocks.0.hook_resid_pre"]
+            [f"blocks.{i}.hook_attn_out" for i in range(GEMMA_1_LAYER)] +
+            [f"blocks.{i}.hook_mlp_out" for i in range(GEMMA_1_LAYER)] +
+            [f"blocks.{GEMMA_1_LAYER}.hook_resid_pre", "blocks.0.hook_resid_pre"]
     )
+
+# --- Gemma2 --- #
+GEMMA_2 = "gemma2"
+GEMMA_2_MODEL_PATH = 'google/gemma-2-2b-it'
+GEMMA_2_LAYER = 20
+GEMMA_2_HOOK_NAMES = (
+            [f"blocks.{i}.ln1_post.hook_normalized" for i in range(GEMMA_2_LAYER)] +
+            [f"blocks.{i}.ln2_post.hook_normalized" for i in range(GEMMA_2_LAYER)] +
+            [f"blocks.{GEMMA_2_LAYER}.hook_resid_pre", "blocks.0.hook_resid_pre"]
+    ) 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """
 Where do steering vectors come from
 """
-from analysis import compare_component_prediction_r2, predict_dot_product_lasso
+from analysis import predict_dot_product_lasso
 from model import ModelBundle
-from consts import GEMMA, GEMMA2, LAYER_GEMMA1, LAYER_GEMMA2
+from consts import GEMMA_1, GEMMA_2
 import os
 import re
 from arg_parser import parse_args
@@ -25,14 +25,14 @@ def main():
     model_bundle = ModelBundle()
 
     # GEMMA and harmfull steering vector
-    if GEMMA in args.models:
-        model_bundle.load_model(GEMMA)
+    if GEMMA_1 in args.models:
+        model_bundle.load_model(GEMMA_1)
         model_bundle.load_steering_vector("harmfull")
         predict_dot_product_lasso(model_bundle=model_bundle)
 
-    # for GEMMA2 all steering vectors in dir content/axbench_chosen_dataset/
-    if GEMMA2 in args.models:
-        model_bundle.load_model(GEMMA2)
+    # GEMMA2 and all steering vectors in dir content/axbench_chosen_dataset/
+    if GEMMA_2 in args.models:
+        model_bundle.load_model(GEMMA_2)
         for steering_vector in get_steering_vector_names_for_gemma2():
             model_bundle.load_steering_vector(steering_vector)
             predict_dot_product_lasso(model_bundle=model_bundle)
