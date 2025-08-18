84e0cd0fc296f130361c681e03ddab6d2f99e637
Shira troyansky <troyansky1@mail.tau.ac.il>
2025-08-18 11:44:17 +0300

    small change in arg parser

--- arg_parser.py
+++ arg_parser.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from consts import GEMMA, GEMMA2
+from consts import GEMMA_1, GEMMA_2
 
 def parse_args():
     parser = ArgumentParser()
@@ -7,8 +7,8 @@ def parse_args():
         "-m",
         "--models",
         nargs="+",
-        choices=[GEMMA, GEMMA2],
-        default=[GEMMA, GEMMA2],
+        choices=[GEMMA_1, GEMMA_2],
+        default=[GEMMA_2],
         help="One or more model names to run",
     )
     return parser.parse_args()
